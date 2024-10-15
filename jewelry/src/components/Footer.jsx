071524eaf9c546cd7a32cf4804a3b51df47048db
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-10-15 13:54:41 -0300

    Ajustes no header, media-query e adição de elementos do footer.

--- jewelry/src/components/Footer.jsx
+++ jewelry/src/components/Footer.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from 'react';
+import styles from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do WhatsApp
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importa o ícone do Instagram
+import { faPix } from '@fortawesome/free-brands-svg-icons';
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
+    <section className={styles.footer}>
       <h1>Todos direitos reservados.</h1>
-    </div>
-  )
+      <FontAwesomeIcon icon={faWhatsapp} />
+      <FontAwesomeIcon icon={faInstagram} />
+      <div>
+        <h2>Aceitamos dinheiro, pix e cartões de crédito</h2>
+      <FontAwesomeIcon icon={faCreditCard} />
+      <FontAwesomeIcon icon={faPix} />
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
