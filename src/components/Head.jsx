8dfa4792720a2d6aabe8e5150bb853689b70be5d
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-12-03 14:03:17 -0300

    Deploy

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,10 +4,8 @@ const Head = ({ title, description }) => {
   React.useEffect(() => {
     document.title = title;
 
-    // Procura pela meta tag de descrição
     let metaDescription = document.querySelector('meta[name="description"]');
 
-    // Se não existir, cria uma nova meta tag
     if (!metaDescription) {
       metaDescription = document.createElement('meta');
       metaDescription.name = 'description';
@@ -18,7 +16,7 @@ const Head = ({ title, description }) => {
     metaDescription.setAttribute('content', description);
   }, [title, description]); // Usar diretamente title e description
 
-  return null; // Retorna null já que o componente não precisa renderizar nada visualmente
+  return null;
 };
 
 export default Head;
