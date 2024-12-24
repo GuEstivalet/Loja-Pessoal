e9dba399f216fbc8cd76b654b137cadab5cd2246
Gustavo Ulysséa Estivalet <131898301+GuEstivalet@users.noreply.github.com>
2024-12-24 09:08:26 -0300

    Ajustes no projeto

--- src/components/SecHero.jsx
+++ src/components/SecHero.jsx
@@ -4,9 +4,9 @@ import styles from './SecHero.module.css';
 const SecHero = () => {
   return (
     <section className={styles.secHero}>
-        <img src='/assets/hero-section.jpg' alt="Imagem de uma mulher atras de uma parede de papel reasgada por ela." />
+        <img src='/assets/image-logo.png' alt="Imagem de uma mulher atras de uma parede de papel reasgada por ela." />
         <div>
-          <h1>Limitless</h1>
+          <h1>Novos tempos, novos lançamentos</h1>
           <p>Às vezes você precisa dar um pequeno grande passo para provar a si mesma que não há limites para quem acredita em seu próprio potencial.</p>
           <a href="/">Confira novos lançamentos</a> 
         </div>
